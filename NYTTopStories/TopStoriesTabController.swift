9d46b61faf91f704f342b23f0577f8660cea4592
Yuliia Engman <yuliia.engman@gmail.com>
2020-02-06 10:49:23 -0500

    Initial Commit

---
+++ NYTTopStories/TopStoriesTabController.swift
@@ -0,0 +1,19 @@
+//
+//  TopStoriesTabController.swift
+//  NYTTopStories
+//
+//  Created by Yuliia Engman on 2/6/20.
+//  Copyright © 2020 Yuliia Engman. All rights reserved.
+//
+
+import UIKit
+
+class TopStoriesTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+    
+    
+}
