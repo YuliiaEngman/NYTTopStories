0e61605c8005c9adfafbc557e115375554fcaa3e
Yuliia Engman <yuliia.engman@gmail.com>
2020-02-06 12:39:29 -0500

    imported a swift packages to the project

---
+++ NYTTopStories/Models/NYTTopStoriesApIClient.swift
@@ -0,0 +1,9 @@
+//
+//  NYTTopStoriesApIClient.swift
+//  NYTTopStories
+//
+//  Created by Yuliia Engman on 2/6/20.
+//  Copyright © 2020 Yuliia Engman. All rights reserved.
+//
+
+import Foundation
