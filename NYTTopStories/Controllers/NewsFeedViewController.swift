4caf2ed3f12330a31f841307c4520ba21a4c9faa
Yuliia Engman <yuliia.engman@gmail.com>
2020-02-06 11:39:15 -0500

    added collection view to VC and created generic cells

--- NYTTopStories/Controllers/NewsFeedViewController.swift
+++ NYTTopStories/Controllers/NewsFeedViewController.swift
@@ -9,10 +9,44 @@
 import UIKit
 
 class NewsFeedViewController: UIViewController {
+    
+    private let newsFeedView = NewsFeedView()
+    
+    override func loadView() {
+        view = newsFeedView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground // white during the day and dark during the dark mode is on
+        newsFeedView.collectionView.delegate = self
+        newsFeedView.collectionView.dataSource = self
+        
+        // register a collectionView cell
+        // we use generic cell
+        newsFeedView.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "articleCell")
     }
 
+}
+
+extension NewsFeedViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+          let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "articleCell", for: indexPath)
+        cell.backgroundColor = .white
+          return cell
+    }
+  
+}
+
+extension NewsFeedViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let maxSize: CGSize = UIScreen.main.bounds.size
+        let itemWidth: CGFloat = maxSize.width
+        let itemHeight: CGFloat = maxSize.height * 0.30
+        return CGSize(width: itemWidth, height: itemHeight)
+    }
 }
---
+++ NYTTopStories/Views/NewsFeedView.swift
@@ -0,0 +1,63 @@
+//
+//  NewsFeedView.swift
+//  NYTTopStories
+//
+//  Created by Yuliia Engman on 2/6/20.
+//  Copyright © 2020 Yuliia Engman. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedView: UIView {
+    
+    public lazy var searchBar: UISearchBar = {
+        let sb = UISearchBar()
+        sb.autocapitalizationType = .none
+        sb.placeholder = "search for article"
+        return sb
+    }()
+    
+    public lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let cv = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        cv.backgroundColor = .blue
+        return cv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super .init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit(){
+        setupSearchBarConstraints()
+        setupLayoutConstraints()
+    }
+    
+    private func setupSearchBarConstraints() {
+        addSubview(searchBar)
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+    
+    private func setupLayoutConstraints() {
+        addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
