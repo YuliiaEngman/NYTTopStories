6d8172f1321bb7abfb1058eaf6a005fe861084a1
Yuliia Engman <yuliia.engman@gmail.com>
2020-02-06 12:53:09 -0500

    imported and updated Network Helper

--- NYTTopStories/Models/NYTTopStoriesApIClient.swift
+++
@@ -1,9 +0,0 @@
-//
-//  NYTTopStoriesApIClient.swift
-//  NYTTopStories
-//
-//  Created by Yuliia Engman on 2/6/20.
-//  Copyright © 2020 Yuliia Engman. All rights reserved.
-//
-
-import Foundation
---
+++ NYTTopStories/NetworkServices/NYTTopStoriesApIClient.swift
@@ -0,0 +1,34 @@
+//
+//  NYTTopStoriesApIClient.swift
+//  NYTTopStories
+//
+//  Created by Yuliia Engman on 2/6/20.
+//  Copyright © 2020 Yuliia Engman. All rights reserved.
+//
+
+import Foundation
+import NetworkHelper
+
+struct NYTTopStoriesApIClient {
+    static func fetchTopStories(for section: String, completion: @escaping (Result<[Article, AppError]>) -> ()) {
+        let endpointURLString = ""
+        guard let url = URL(string: endpointURLString) else {
+            completion(.failure(.badURL(endpointURLString)))
+            return
+        }
+        let request = URLRequest(url: url)
+        NetworkHelper.shared.performDataTask(with: request) {(result) in
+            
+            swithch resulet {
+                case .failure(let appError):
+                completion(.failure(.networkClientError(appError)))
+                case .success(let data):
+                do {
+                let topStories = try JSONDecoder().decode(TopStories.self, from: data)
+                completion(.success(topStories).results)
+                } catch {
+                completion(.failure(.decodingError(error)))
+                }
+            }
+        }
+}
